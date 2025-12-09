c2c4684124ceca0b1be7d29368bde9639cf7f3f5
Muhiddin Kurbonov <muhiddindc@gmail.com>
2025-12-09 17:49:18 -0600

    fix merge conflict

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-    },
-  },
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-  },
-=======
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
->>>>>>> p2group3
 })
